440d068ec90ae13ee2df7171f1974b4f69e547ee
hoaile2k <>
2024-08-25 03:14:48 +0700

    integrate flow join room+ chat

--- socket-io-server.js
+++ socket-io-server.js
@@ -1,31 +1,35 @@
 
 const {Server} = require("socket.io");
 const connectSocket = (server)=> {
-
-    let mapUserInfoUserId = {};
-
+    let mapRoomById = {};
+    let mapUserInfoById = {};
+    let mapSocketOnRoom = {};
     const io = new Server(server);
     io.on('connection', (socket) => {
         console.log("connection socket", socket.id);
-        let mapListChatOnRoom = {};
-        let mapListUserOnRoom = {};
         socket.on('disconnect', (s) => {
             console.log(`disconnect: ${socket.id}`);
         });
 
         socket.on("request-register", ({userId, userName})=>{
             console.log(`User ${userId} register`,socket.id);
-            mapUserInfoUserId[userId] = {
+            mapUserInfoById[userId] = {
                 userId: userId,
-                userName: userName
+                userName: userName,
+                listFriend: [],
+                listRoom: []
             }
-            io.to(socket.id).emit("register-success", mapUserInfoUserId[userId]);
+            io.to(socket.id).emit("register-success", mapUserInfoById[userId]);
         });
 
         socket.on("request-login", ({userId})=>{
-            console.log(`User ${userId} login`, mapUserInfoUserId);
-            if(mapUserInfoUserId[userId]){
-                io.to(socket.id).emit("login-success", mapUserInfoUserId[userId]);
+            console.log({userId: userId});
+            console.log(`User ${userId} login`, mapUserInfoById);
+            if(mapUserInfoById[userId]){
+                let listRoomInfo = [];
+                mapUserInfoById[userId].listRoom.forEach(roomId => listRoomInfo.push(mapRoomById[roomId]));
+                mapUserInfoById[userId].listRoomInfo = listRoomInfo;
+                io.to(socket.id).emit("login-success", mapUserInfoById[userId]);
             }else {
                 io.to(socket.id).emit("login-failed", {
                     message: "user not found"
@@ -33,45 +37,79 @@ const connectSocket = (server)=> {
             }
         })
 
-
-        socket.on("join", ({username, roomId, userId}) => {
-            if (!mapListUserOnRoom[roomId]) {
-                mapListUserOnRoom[roomId] = [];
+        socket.on("request-create-room", ({roomId, roomName, userId})=>{
+            console.log("request-create-room", {roomId, roomName, userId});
+            if(mapRoomById[roomId]){
+                io.to(socket.id).emit("create-room-failed", {message: "This roomId is existed"});
+            }else {
+                console.log("user " + mapUserInfoById[userId].userName + "create room " + roomName);
+                mapRoomById[roomId] = {
+                    roomId: roomId,
+                    roomName: roomName,
+                    listUser: [],
+                    listRecentChat: [],
+                    lastMsg: "N/A"
+                }
+                io.to(socket.id).emit("create-room-success", {
+                    message: "room: " + roomName + " is created successfully",
+                    newRoom: mapRoomById[roomId]
+                });
             }
-            const path = "room_" + roomId;
-            console.log("user " + userId);
-            //Join room
-            socket.join(path);
-            //Send private message when join success
-            const userInfo = {
-                username: username,
-                avatar: 0,
-                userId: userId,
-            }
-            console.log("userInfo", userInfo)
-            io.to(socket.id).emit("join-room-success", {
-                roomId: roomId,
-                listUserOnRoom: mapListUserOnRoom[roomId],
-                userInfo: userInfo
-            });
-            //Send present message user join room
-            io.in(path).emit("user-join-room", userInfo);
-            mapListUserOnRoom[roomId].push(userInfo);
         })
-        socket.on("send-chat", ({username, content, roomId, userId}) => {
-            if (!mapListChatOnRoom[roomId]) {
-                mapListChatOnRoom[roomId] = [];
+
+        socket.on("request-join-room", ({roomId, userId})=>{
+            console.log("user " + mapUserInfoById[userId].userName + " join room " + roomId);
+            if(mapRoomById[roomId]){
+                socket.join(roomId);
+                if(mapUserInfoById[userId].listRoom.indexOf(roomId) === -1) {
+                    mapUserInfoById[userId].listRoom.push(roomId);
+                }
+                if(mapRoomById[roomId].listUser.indexOf(userId) === -1) {
+                    mapRoomById[roomId].listUser.push(userId);
+                }
+                io.to(socket.id).emit("join-room-success", {
+                    roomId: roomId,
+                    roomName: mapRoomById[roomId].roomName,
+                    listUser: _getListUserNameByListUserId(mapRoomById[roomId].listUser),
+                    listRecentChat: mapRoomById[roomId].listRecentChat
+                });
+                io.in(roomId).emit("new-user-join-room", mapUserInfoById[socket.id]);
+                !mapSocketOnRoom[roomId] && (mapSocketOnRoom[roomId] = []);
+                if(!_checkSocketExistRoom(socket.id, mapSocketOnRoom)) {
+                    mapSocketOnRoom[roomId].push(socket.id);
+                }
+            }else {
+                io.to(socket.id).emit("join-room-failed", {message: "This roomId isn't existed"});
             }
-            const path = "room_" + roomId;
-            console.log()
-            const chatInfo = {
-                username: username,
-                content: content,
-                userId: userId
+
+        })
+
+        socket.on("send-chat", ({roomId, content, userId})=>{
+            console.log("send-chat", roomId, mapRoomById, userId)
+            const arrSocketOnRoom = mapSocketOnRoom[roomId] || [];
+            const newChat = {
+                userChatId: userId,
+                userChat: mapUserInfoById[userId].userName,
+                content: content
             }
-            mapListChatOnRoom[roomId].push(chatInfo);
-            socket.in(path).emit("chat-message", mapListChatOnRoom[roomId]);
+            mapRoomById[roomId].listRecentChat.push(newChat);
+            mapRoomById[roomId].lastMsg = content;
+            console.log(mapSocketOnRoom, roomId, mapSocketOnRoom[roomId])
+            arrSocketOnRoom.forEach(sId => {
+                io.to(sId).emit("new-chat", newChat);
+            })
         })
     });
+    function _checkSocketExistRoom(socketId, mapSocket) {
+        return Object.keys(mapSocket).indexOf(socketId) >= 0;
+    }
+
+    function _getListUserNameByListUserId(listUserId) {
+        let listUserName = [];
+        listUserId.forEach(userId => {
+            listUserName.push(mapUserInfoById[userId].userName);
+        })
+        return listUserName;
+    }
 }
 module.exports.connectSocket = connectSocket;
